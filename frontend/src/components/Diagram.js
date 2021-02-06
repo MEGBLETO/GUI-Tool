30974d2d7ea3c0cc137a6e100e4c11c5b49e523c
MEGBLETO carnel <carnelilordwi@gmail.com>
2021-02-06 15:11:45 +0100

    small improvents to my UI

--- frontend/src/components/Diagram.js
+++ frontend/src/components/Diagram.js
@@ -3,12 +3,16 @@ import Xarrow from 'react-xarrows'
 
 const Diagram = () => {
   return (
-    <div className="diagram">
+ <div className="diagram">
       <div className="A" id="A">A</div>
-      <div className="B">B</div>
-      <div className="C">C</div>
-      <div className="D">D</div>
-    </div>
+      <div className="B" id="B">B</div>
+      <div className="C" id="C">C</div>
+      <div className="D" id="D">D</div>  
+      <Xarrow start="A" end="B"/>
+      <Xarrow start="B" end="D"/>
+      <Xarrow start="B" end="C"/>
+
+</div>
   )
 }
 
--- frontend/src/components/Entete.js
+++ frontend/src/components/Entete.js
@@ -4,6 +4,7 @@ const Entete = () => {
   return (
     <div className="entete">
       <h1>GUI <span>Tool</span></h1>
+      <button id="btn">Se Connecter</button>
     </div>
   )
 }
